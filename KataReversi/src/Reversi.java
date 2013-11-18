1924405aedd72b0619a594bba837538c986921bd
Max <maxime.arbez@gmail.com>
2013-11-18 16:51:09 +0100

    Réfactoring partie 2 : gestion des sauts de ligne

--- KataReversi/src/Reversi.java
+++ KataReversi/src/Reversi.java
@@ -10,33 +10,21 @@ public class Reversi {
 		String output= "";
 		for (int i = 0; i < tableau.length; i++) {
 			Ligne ligne = new Ligne();
-			/*if(joueurCourant.equals("B")){
-				if(tableau[i].indexOf("B")<tableau[i].indexOf("W.") && tableau[i].indexOf("B")!=-1){
-					tableau[i] = tableau[i].replace("W.","W0");
-				}
-				if(tableau[i].indexOf("B")>tableau[i].indexOf(".W") && tableau[i].indexOf("B")!=-1){
-					tableau[i] = tableau[i].replace(".W","0W");
-				}
-			}
-			else{
-				//------------------------Gestion des lignes Joueur W----------------
-
-				if(tableau[i].indexOf("W")<tableau[i].indexOf("B.") && tableau[i].indexOf("W")!=-1){
-					tableau[i] = tableau[i].replace("B.","B0");
-				}
-				if(tableau[i].indexOf("W")>tableau[i].indexOf(".B") && tableau[i].indexOf("W")!=-1){
-					tableau[i] = tableau[i].replace(".B","0B");
-				}
-			}*/					    	
-			//-------------------Gestion des sauts de lignes à la sortie-------------------------
-			if (i==tableau.length-1){
-				output = output + ligne.retournePosition(joueurCourant,tableau[i]);
-			}
-			else
-				output = output + ligne.retournePosition(joueurCourant,tableau[i]) + "\n";
+		    	
+			output = sortieTableau(tableau, joueurCourant, output, i, ligne);
 		}
 		System.out.println(output);
 		return output;
+	}
+	
+	
+	//-------------------Gestion des sauts de lignes à la sortie-------------------------
+	public String sortieTableau(String[] tableau, String joueurCourant,
+			String output, int i, Ligne ligne) {
+		if (i==tableau.length-1){
+			return output + ligne.retournePosition(joueurCourant,tableau[i]);
+		}
+			return output + ligne.retournePosition(joueurCourant,tableau[i]) + "\n";
 	}
 
 	/*-----------------------Savoir si le dernier carctère est B ou W-----------------------------*/
